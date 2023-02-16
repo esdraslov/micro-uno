db82197cf7ee06bf7b970b3a7d31839276e79bff
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-02-16 17:01:28 -0300

    added new functions(10 functions use others extentions)

--- main.ts
+++ main.ts
@@ -1,3 +1,44 @@
-basic.forever(function () {
-	
-})
+namespace microUno {
+    export function unoLed (x: number, y: number){
+        for (let i = 0; i < y; i++) {
+            led.plot(x, i)
+            basic.pause(1000)
+        }
+    }
+    export function unUnoLed(x: number, y: number) {
+        for (let i = 0; i < y; i++) {
+            led.unplot(x, i)
+            basic.pause(1000)
+        }
+    }
+    export function unoConnectServoP0Angle(angle: number) {
+        servos.P0.setAngle(angle)
+    }
+    export function unoConnectServoP1Angle(angle: number) {
+        servos.P1.setAngle(angle)
+    }
+    export function unoConnectServoP2Angle(angle: number) {
+        servos.P2.setAngle(angle)
+    }
+    export function unoConnectSonarP0P0() {
+        sonar.ping(DigitalPin.P0, DigitalPin.P0, PingUnit.MicroSeconds)
+    }
+    export function unoConnectSonarP0P1() {
+        sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.MicroSeconds)
+    }
+    export function unoConnectSonarP1P1() {
+        sonar.ping(DigitalPin.P0, DigitalPin.P0, PingUnit.MicroSeconds)
+    }
+    export function unoConnectSonarP1P0() {
+        sonar.ping(DigitalPin.P1, DigitalPin.P0, PingUnit.MicroSeconds)
+    }
+    export function unoConnectSonarP0P2() {
+        sonar.ping(DigitalPin.P0, DigitalPin.P2, PingUnit.MicroSeconds)
+    }
+    export function unoConnectSonarP2P0() {
+        sonar.ping(DigitalPin.P2, DigitalPin.P0, PingUnit.MicroSeconds)
+    }
+    export function unoConnectSonarP2P2() {
+        sonar.ping(DigitalPin.P0, DigitalPin.P0, PingUnit.MicroSeconds)
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
-// testes vão aqui; isto não será compilado quando este pacote for usado como uma extensão.
+microUno.unoLed(1, 1)
+microUno.unoConnectSonarP0P0()
+microUno.unoConnectSonarP1P1()
+basic.pause(9000)
+microUno.unUnoLed(1, 1)
